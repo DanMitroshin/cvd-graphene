0d8fc89740ed2f7bb0e0c94727d95e07bb238adf
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-10-05 11:43:08 +0300

    Test full screen and close buttons

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import sys
 from PyQt6.QtWidgets import (
-    QMainWindow, QApplication, QListWidget,
+    QMainWindow, QApplication, QListWidget, QVBoxLayout,
     QLabel, QCheckBox, QComboBox, QLineEdit, QStyle,
-    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider, QWidget, QHBoxLayout, QFrame
+    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider, QWidget, QHBoxLayout, QFrame, QPushButton
 )
 from PyQt6 import QtWidgets
 from PyQt6.QtCore import Qt, QMetaType, QRect
@@ -34,16 +34,17 @@ class MainWindow(QMainWindow):
         # self.line.setGeometry(QRect(60, 110, 751, 20))
         self.line.setFrameShape(QFrame.Shape.HLine)
         self.line.setFrameShadow(QFrame.Shadow.Sunken)
-        self.line.setSizePolicy(QtWidgets.QSizePolicy.Policy.Preferred,
+        self.line.setSizePolicy(QtWidgets.QSizePolicy.Policy.Minimum,
                                 QtWidgets.QSizePolicy.Policy.Minimum)
 
         self.label = QLabel()
         # self.label.setStyleSheet('color:"red";font-size: 32;')
         self.label.setStyleSheet("""
         QWidget {
-            border: 20px solid black;
+            border: 2px solid black;
             border-radius: 10px;
             background-color: rgb(255, 255, 255);
+            font-size: 32px;
             }
         """)
 
@@ -51,18 +52,45 @@ class MainWindow(QMainWindow):
         self.input.textChanged.connect(self.label.setText)
 
         layout = QHBoxLayout()
+        layout_v = QVBoxLayout()
         layout.addWidget(self.line)
         layout.addWidget(self.input)
         layout.addWidget(self.label)
 
+        self.button_close = QPushButton("нажать для закрытия программы")
+        self.button_close.clicked.connect(self.close)
+        self.button_close.setStyleSheet("""
+        QPushButton {
+            height: 200px;
+            font-size: 20px;
+        }
+        """)
+        layout_v.addLayout(layout)
+        self.button = QPushButton("Press Me!")
+        self.button.setStyleSheet("""
+                QPushButton {
+                    height: 100px;
+                    font-size: 16px;
+                }
+                """)
+        self.counter = 0
+        self.button.clicked.connect(self.click_press)
+
+        layout_v.addWidget(self.button)
+        layout_v.addWidget(self.button_close)
+
         container = QWidget()
-        container.setLayout(layout)
+        container.setLayout(layout_v)
 
         # Устанавливаем центральный виджет Window.
         self.setCentralWidget(container)
 
+    def click_press(self):
+        self.counter += 1
+        self.label.setText(f"PRESSED: {self.counter}")
 
 app = QApplication(sys.argv)
 w = MainWindow()
-w.show()
+w.showFullScreen()
+# w.show()
 app.exec()
