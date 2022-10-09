eda9d1554356d9774ce6bf93eeb77e9ca8c0db0a
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-10-09 14:35:29 +0300

    Update valve lines

--- Structure/dialog_ui/MainBlockWidget/PressureBlock/ValveControlWidget/__init__.py
+++ Structure/dialog_ui/MainBlockWidget/PressureBlock/ValveControlWidget/__init__.py
@@ -42,46 +42,13 @@ class ValveControlButton(QPushButton):
         self.setLayout(self.layout)
         self.setObjectName("button_valve_control")
         self.setStyleSheet(button_style)
+        self.setAttribute(QtCore.Qt.WA_StyledBackground, True)
 
         # self.button = QPushButton()
         # self.button.setObjectName("button_valve_control")
         # self.button.setStyleSheet(button_style)
         self.w = ValveLines()
         self.layout.addWidget(self.w)
-
-    def paintEvent1(self, event):
-        try:
-            qp = QPainter()
-            # qp.begin(self.button)
-            qp.begin(self)
-
-            # a = randint(10, 100)
-            # d = a * math.tan(math.radians(30))
-            x, y = 0, 20
-
-            pos_1 = QPointF(x, y)
-            pos_2 = QPointF(90, y)
-
-            pos_3 = QPointF(x, y)
-            pos_4 = QPointF(90, y + 30)
-
-            qp.setBrush(QColor(0, 0, 0))
-            path = QPainterPath()
-            path.moveTo(pos_1)
-            path.lineTo(pos_2)
-
-            path2 = QPainterPath()
-            path2.moveTo(pos_4)
-            path2.lineTo(pos_3)
-
-            # qp.rotate(7)
-            qp.drawPath(path)
-            qp.drawPath(path2)
-
-
-            qp.end()
-        except Exception as e:
-            print("Draw line error", e)
 
 
 class ValveLines(QWidget):
@@ -96,8 +63,16 @@ class ValveLines(QWidget):
             height: 90px;
             min-height: 90px;
             border-radius: 45px;
+            transform: [{rotate: 60deg}];
             background-color: #FFFFFF;
         """)
+        s = """
+            background-color: #FFFFFF;
+        """
+        s = ""
+        s += f"min-height: {int(90 / 2 * (1 + 2 ** 0.5 / 2))}px;"
+        self.setStyleSheet(s)
+        self.setAttribute(QtCore.Qt.WA_StyledBackground, True)
         #
         # # self.button = QPushButton()
         # # self.button.setObjectName("button_valve_control")
