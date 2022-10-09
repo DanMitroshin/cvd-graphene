d343375238cf88679f4bc60b410bf2a03dcb2b7f
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-10-09 15:40:30 +0300

    Update test for vakumetr

--- Tests/devices/vakumetr.py
+++ Tests/devices/vakumetr.py
@@ -1,15 +1,17 @@
 import wiringpi
+from time import sleep
 
 
 def test_1():
     SEND_STR = '0010MV00D\r'
-    serial = wiringpi.serialOpen('/dev/ttyAMA1', 9600)  # Requires device/baud and returns an ID
+    serial = wiringpi.serialOpen('/dev/ttyAMA0', 9600)  # Requires device/baud and returns an ID
     # serial = wiringpi.serialOpen('/dev/ttyACM0', 9600)  # Requires device/baud and returns an ID
     print("Serial:", serial)
     ans = wiringpi.serialPuts(serial, SEND_STR)
     print("Answer:", ans)
     b = ""
     counter = 0
+    sleep(2000)
     while True:
         b = wiringpi.serialGetchar(serial)
         counter += 1
