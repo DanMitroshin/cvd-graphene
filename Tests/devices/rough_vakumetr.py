6cf5afd27645d62b0ebf49dc49e4bfcae12987f9
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-10-14 00:16:35 +0300

    Update test for rough_vakumetr

--- Tests/devices/rough_vakumetr.py
+++ Tests/devices/rough_vakumetr.py
@@ -2,6 +2,7 @@
 
 try:
     import wiringpi
+    import spidev
 except:
     pass
 # import serial
@@ -19,10 +20,33 @@ def test_rough_vakumetr_1():
         print("|> Receive:", receive_data)
 
 
+def test_rough_vakumetr_2():
+    SPIchannel = 3
+    SPIspeed = 100000
+    spi = spidev.SpiDev()
+    spi.open(SPIchannel, 0)
+    spi.max_speed_hz = 50000
+    print("Connected!")
+    # spi.lsbfirst = False
+    # spi.cshigh = False
+    # spi.mode = 0b01
+    # spi.bits_per_word = 8
+    send_data = b'000'
+
+    txData = [0x00]
+    rxData = spi.xfer(txData)
+    print("Receive:", end=' ')
+    for i in range(len(rxData)):
+        print(hex(rxData[i]), end=' ')
+    print('')
+
+    spi.close()
+
+
 if __name__ == "__main__":
     print("TEST 1 ===>")
     try:
-        test_rough_vakumetr_1()
+        test_rough_vakumetr_2()
         print("TEST 1 ===> PASSED")
     except Exception as e:
         print("[ERROR]", e)
