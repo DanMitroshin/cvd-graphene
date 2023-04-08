7278a82c9b6ba904f54205deff19d5b91ff637d6
DanMitroshin <alfocintavr@mail.ru>
2023-04-08 22:25:18 +0300

    Main block from base component

--- Structure/dialog_ui/MainBlockWidget/__init__.py
+++ Structure/dialog_ui/MainBlockWidget/__init__.py
@@ -1,7 +1,5 @@
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import QPushButton, QWidget, QGridLayout, QHBoxLayout, QVBoxLayout, QLabel
-
 from coregraphene.conf import settings
+from grapheneqtui.structures import BaseMainBlockWidget
 # from .PressureBlock import PressureBlock, ValvesControlBlock
 from .PressureBlock import ValvesControlBlock
 from .PressureControlBlock import PressureControlBlock
@@ -9,18 +7,9 @@ from .TemperatureBlock import TemperatureBlock
 from .styles import styles
 
 
-class MainBlockWidget(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent=parent)
+class MainBlockWidget(BaseMainBlockWidget):
 
-        self.layout = QHBoxLayout()
-        self.setLayout(self.layout)
-        self.setObjectName("main_block_widget")
-        self.setStyleSheet(styles.container)
-        self.setAttribute(QtCore.Qt.WA_StyledBackground, True)
-        # self.setStyleSheet("* {background-color: rgb(0, 0, 255);}")
-
-        # self.pressure_block = ValvesControlBlock()
+    def set_layout_content(self):
         self.pressure_block = ValvesControlBlock(
             gases_configuration=settings.VALVES_CONFIGURATION,
             default_sccm_value=settings.MAX_DEFAULT_SCCM_VALUE,
@@ -32,37 +21,3 @@ class MainBlockWidget(QWidget):
 
         self.temperature_block = TemperatureBlock()
         self.layout.addWidget(self.temperature_block)
-
-        self.inactive_widget = QWidget(self)
-        self.inactive_widget.setObjectName("inactive_widget")
-        self.inactive_widget.setStyleSheet(styles.inactive_widget)
-        self.inactive_widget.hide()
-
-        self.recipe_labels_layout = QVBoxLayout()
-        self.last_recipe_step = QLabel()
-        self.current_recipe_step = QLabel()
-        self.recipe_labels_layout.addWidget(self.current_recipe_step)
-        self.recipe_labels_layout.addWidget(self.last_recipe_step)
-
-        self.current_recipe_step_text = ""
-        self.last_recipe_step_text = ""
-
-        self.inactive_widget.setLayout(self.recipe_labels_layout)
-
-    def deactivate_interface(self):
-        self.inactive_widget.show()
-
-    def activate_interface(self):
-        self.current_recipe_step_text = ""
-        self.last_recipe_step_text = ""
-        self.current_recipe_step.setText(self.current_recipe_step_text)
-        self.last_recipe_step.setText(self.last_recipe_step_text)
-
-        self.inactive_widget.hide()
-
-    def set_current_step(self, step=""):
-        self.last_recipe_step_text = self.current_recipe_step_text
-        self.current_recipe_step_text = step
-
-        self.current_recipe_step.setText(self.current_recipe_step_text)
-        self.last_recipe_step.setText(self.last_recipe_step_text)
