8f30680f2d4db9aa0e533d044729eab339f54717
DanMitroshin <alfocintavr@mail.ru>
2024-02-04 19:43:10 +0300

    Settings update

--- Core/settings/__init__.py
+++ Core/settings/__init__.py
@@ -6,6 +6,7 @@ IMPORT HERE ALL NECESSARY SETTINGS FOR YOUR DEVICE AND CONNECTIONS
 
 from .actions import *
 from .raspberry import *
+import platform
 
 
-LOCAL_MODE = True
+LOCAL_MODE = platform.system() != 'Linux'
--- Core/settings/raspberry.py
+++ Core/settings/raspberry.py
@@ -65,16 +65,6 @@ VALVES_CONFIGURATION = [
         'DAC_ADDRESS': 0,  # RRG ADDRESS FOR SPI DAC [SET VALUE] (from 0 to 7: 000, 001, ..., 111)
         'VAKUMETR_ADDRESS': 0,  # VAKUMETR ADDRESS FOR READING PRESSURE IN BALLOON
     },
-    {
-        'PORT': 24, "NAME": "CH_4", "IS_GAS": True,
-        'CONTROLLER_VOLTAGE_RATIO': 1, 'CONTROLLER_VOLTAGE_SHIFT': 0,
-        'ADDRESS': 1, 'DAC_ADDRESS': 1, 'VAKUMETR_ADDRESS': 4,
-    },
-    {
-        'PORT': 23, "NAME": "H_2", "IS_GAS": True,
-        'CONTROLLER_VOLTAGE_RATIO': 1, 'CONTROLLER_VOLTAGE_SHIFT': 0,
-        'ADDRESS': 4, 'DAC_ADDRESS': 7, 'VAKUMETR_ADDRESS': 5,
-    },
         {
         'PORT': 25,
         "NAME": "C_2H_2",
@@ -83,9 +73,19 @@ VALVES_CONFIGURATION = [
         'ADDRESS': 2, 'DAC_ADDRESS': 2, 'VAKUMETR_ADDRESS': 2,
     },
         {
+        'PORT': 24, "NAME": "H_2", "IS_GAS": True,
+        'CONTROLLER_VOLTAGE_RATIO': 1, 'CONTROLLER_VOLTAGE_SHIFT': 0,
+        'ADDRESS': 1, 'DAC_ADDRESS': 1, 'VAKUMETR_ADDRESS': 4,
+    },
+        {
         'PORT': 4, "NAME": "N_2", "IS_GAS": True,
         'CONTROLLER_VOLTAGE_RATIO': 1, 'CONTROLLER_VOLTAGE_SHIFT': 0,
         'ADDRESS': 3, 'DAC_ADDRESS': 3, 'VAKUMETR_ADDRESS': 3,
+    },
+        {
+        'PORT': 23, "NAME": "CH_4", "IS_GAS": True,
+        'CONTROLLER_VOLTAGE_RATIO': 1, 'CONTROLLER_VOLTAGE_SHIFT': 0,
+        'ADDRESS': 4, 'DAC_ADDRESS': 7, 'VAKUMETR_ADDRESS': 5,
     },
     # {'PORT': 7, "NAME": "O_2", "IS_GAS": True},
     # {'PORT': 8, "NAME": "N_2", "IS_GAS": True},
